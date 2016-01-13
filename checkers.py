73e2af7003ec50c56369f1737bad169158421665
Kevin Luong <k5luong@uwaterloo.ca>
2016-01-13 17:06:39 -0500

    added the main loop and win check

--- checkers.py
+++ checkers.py
@@ -3,7 +3,8 @@ import string
 class CheckerBoard(object):
     # Creates the board array and populates it with checker piece objects
 
-    def __init__(self):
+    def __init__(self, ):
+        #create and label the board
         self.board = []
 
         for x in range(9):
@@ -13,22 +14,32 @@ class CheckerBoard(object):
             for i in range(9):
                 row.append('-');
 
-        #for column 0 populate starting from index 1 with numbers 1 through 8
-        
+        #label the x axis with capital letters from A through H
         for x in range (1,9):
             self.board[0][x] = string.uppercase[x-1]
 
+        #label the y axis with numbers from 1 through 8
         a = 1;
         for y in range (1,9):
             self.board[y][0] = a
             a+=1
 
-        #for row 0 populate starting from index 1 with letters A through H
-        #populate everything else with - 
+        for x in range(1,9,2):
+            self.board[1][x] = 'x'
+            self.board[2][x+1] = 'x'
+            self.board[3][x] = 'x'
 
-    def moveA(self,position):
+        for x in range(1,9,2):
+            self.board[6][x] = 'o'
+            self.board[7][x+1] = 'o'
+            self.board[8][x] = 'o'
+
+        pieces1 = pieces2 = 12
+
+    def moveA(self,position, piece):
         """example: A0B1 moves the piece located at A0 to B1"""
-        #defines a function to move the piece 
+        #defines a function to move the piece to the location
+        #piece allows one player to only control one type of piece
 
 
         #while position.length != 4:
@@ -73,12 +84,34 @@ class CheckerBoard(object):
         #print every element in the area with a break between rows for player B
         pass
 
+    def checkWin(self):
+        #return true if there are no more pieces for one player
+        if pieces1 == 0:
+            print("Player 2 wins!")
+            return True
 
-game=CheckerBoard()
-game.printBoard()
+        elif pieces2 == 0:
+            print("Player 1 wins!")
+            return True
+        #else return false
+
+        return False
 
 
+def main():
+    """ Initialize the game and run the main loop"""
+    game= CheckerBoard()
+    game.printBoard()
 
+    while(checkWin == False):
+        #continue running the game loop until someone has won the game
+
+        #implement the turn system, player 1 can only move pieces o or O
+        #player 2 can only move pieces x or X
+    return
+
+
+main()
 
 
 
